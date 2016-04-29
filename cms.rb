c845c1384779fb4361dbae2c5bd988e17de3c252
KatherineEbel <kathyebel@me.com>
2016-04-29 10:19:12 -0400

    added minitest gem for testing

--- cms.rb
+++ cms.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
-require 'pry'
 get '/' do
   @files = Dir.foreach('data').reject { |filename| filename == '.' || filename == '..' }
   erb :index, layout: :layout
@@ -10,6 +9,5 @@ end
 get "/:filename" do
   file_path = "data/" + params[:filename]
   headers["Content-Type"] = "text/plain"
-  binding.pry
   @file = File.read file_path
 end
