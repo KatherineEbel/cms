64e5704f632d1ef2ddddf18a834b7ef5a2a4d9f4
KatherineEbel <kathyebel@me.com>
2016-04-29 11:35:35 -0400

    completed requirements for handling non-existant docs

--- cms.rb
+++ cms.rb
@@ -1,13 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
-get '/' do
+require 'pry'
+
+before do 
   @files = Dir.foreach('data').reject { |filename| filename == '.' || filename == '..' }
+end
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+get '/' do
   erb :index, layout: :layout
 end
 
 get "/:filename" do
   file_path = "data/" + params[:filename]
-  headers["Content-Type"] = "text/plain"
-  @file = File.read file_path
+  if File.exist? file_path 
+    headers["Content-Type"] = "text/plain"
+    File.read file_path
+  else
+    session[:error] = "#{params[:filename]} not found"
+    redirect '/'
+  end
 end
